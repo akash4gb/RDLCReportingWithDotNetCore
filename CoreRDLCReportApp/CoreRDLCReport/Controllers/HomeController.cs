ffa5adaea3d5358e79bfa6072a7774c4c6ab850c
Md. Shahadat Khan Akash <akash4gb@gmail.com>
2022-09-26 15:43:25 +0600

    Created CoreMVC type project.

---
+++ CoreRDLCReportApp/CoreRDLCReport/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using CoreRDLCReport.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoreRDLCReport.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly IWebHostEnvironment _webHostEnvironment;
+
+    public HomeController(IWebHostEnvironment webHostEnvironment)
+    {
+        _webHostEnvironment = webHostEnvironment;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Print()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
